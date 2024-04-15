0dc6c4a021942917398868da3b4520e490dc2b43
dhiasalah <med.dhia.sala@gmail.com>
2024-04-16 00:29:24 +0200

    offTimeMaster Updated4

--- src/pages/Home/components/Test.js
+++ src/pages/Home/components/Test.js
@@ -1,6 +1,10 @@
 import video from "../../../assets/video/video1.mp4"
 import { Link } from 'react-router-dom'
+import { useSelector } from "react-redux"
 export const Test = () => {
+  const user=useSelector(state=>state.userState.user);
+  const user11=useSelector(state=>state.userState.loggedIn);
+
   return (
     <section className="relative h-screen flex flex-col items-center justify-center text-center text-white mb-6 ">
         <div className="video-docker absolute top-0 left-0 w-full h-full overflow-hidden">
@@ -12,7 +16,7 @@ export const Test = () => {
             
           <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight  md:text-5xl lg:text-6xl text-white">Get back to growth with <span className="text-blue-600 dark:text-blue-500">the world's #1 </span>Leave Management System</h1>
           <p className="text-lg font-normal  lg:text-xl text-gray-300"> digitize your leave management process with OffTimeMaster.</p>
-          <Link to="/login" type="button" className="text-white max-w-32 bg-blue-700 hover:bg-blue-100 hover:text-slate-900 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-base px-5 py-2.5 mr-2 mb-2  focus:outline-none dark:focus:ring-blue-800">Get Started</Link>
+          <Link to={`${!user11?"/login" :(user.role==="employe"?"/employe":"/admin/dashboard")} `} type="button" className="text-white max-w-36 bg-blue-700 hover:bg-blue-100 hover:text-slate-900 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-base px-5 py-2.5 mr-2 mb-2  focus:outline-none dark:focus:ring-blue-800">{!user11?"Get Started":"Go to Account"}</Link>
 
 
         </div>
--- src/pages/admin/AdminList.js
+++ src/pages/admin/AdminList.js
@@ -29,7 +29,7 @@ export const AdminList = () => {
         getPosts();
        
       },[adminQuery,toggle])
-
+      
 
 
 
