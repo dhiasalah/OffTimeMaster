19b48d0349e064de46143822258d7d08f4aa9566
DhiaSalahStage <med.dhia.salah.stage@gmail.com>
2024-08-08 15:02:23 +0100

    update

--- src/pages/employee/EmployeeHome.js
+++ src/pages/employee/EmployeeHome.js
@@ -299,7 +299,7 @@ export const EmployeeHome = () => {
         )}
       </div>
 
-      <div className="max-w-xl p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-2">
+      <div className="max-w-xl p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-2 max-h-96 overflow-y-scroll">
         <div className="flex items-center gap-2 px-4">
           <div className="text-green-300">
             <svg
